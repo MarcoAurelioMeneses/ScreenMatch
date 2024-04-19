66aec82dae4b92596d771fe3bb5a254a43ee839f
MarcoAurelioMeneses <marcobrmeneses02@outlook.com>
2024-04-19 16:31:58 -0300

    New commit

--- src/main/java/br/com/marco/screenmactch/services/ConsultarChatGPT.java
+++ src/main/java/br/com/marco/screenmactch/services/ConsultarChatGPT.java
@@ -6,7 +6,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultarChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("API-KEY");
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("text-davinci-003")
